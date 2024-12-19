1c93be0fb5eef05d892a4fee1ceddc94cc4a81fc
Gavin Houston <gavin2024@Gavinspc-Kali.lan>
2024-12-18 18:51:38 -0600

    ops17.sh

---
+++ Ops Challenge's/ops17.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Challenge today is to create a script that list all the devices on the network and ask the user to ping one of the ip address.
+# There is a few different ways to list all deivices on your network you could use an arp command or an nmap command.
+# We will run the a command that prints all address then ask the user to ping a specific one by entering an ip address.
+
+echo "Listing devices on the network: ..."
+nmap -sn 192.168.1.0/24 | grep "Nmap scan report for" | awk '{print $5}'
+
+echo "Enter an IP adress to ping;"
+read ip_address
+
+ping -c 4 "$ip_adress"
