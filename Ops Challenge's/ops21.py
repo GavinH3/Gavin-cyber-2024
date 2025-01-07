b0329f66495bbb87c6a042ee1e02521d9128d6de
Gavin Houston <gavin2024@Gavinspc-Kali.lan>
2025-01-06 18:54:57 -0600

    ops21.py

---
+++ Ops Challenge's/ops21.py
@@ -0,0 +1,38 @@
+# Objectives
+# Create if statements using these logical conditionals below. Each statement should print information to the screen depending on if the condition is met.
+a=5
+b=5
+# Equals: a == b
+if a == b:
+  print("a is equal to b.")
+# Not Equals: a != b
+elif a != b:
+  print("a is not equal to b.")
+# Less than: a < b
+elif a<b:
+  print("a is less then b.")
+# Less than or equal to: a <= b
+elif a<= b:
+  print("a is less than or equal to b.")
+# Greater than: a > b
+elif a > b:
+  print("a is greater than b.")
+# Greater than or equal to: a >= b
+elif a>= b:
+  print("a is greater than or equal to b.")
+# Create an if statement using a logical conditional of your choice and include elif keyword that executes when other conditions met or and else when no condition is met.
+
+else:
+  print("no condition was met.")
+
+
+# Stretch Goals (Optional Objectives)
+# Pursue stretch goals if you are a more advanced user or have remaining lab time.
+
+# Create an if statement with two conditions by using and between conditions.
+
+# Create an if statement with two conditions by using or between conditions.
+
+# Create a nested if statement.
+
+# Hint:  a = int(input("Enter a number "))
