bd36685026ffa8fdc0ab9076aad7edd37e1d6d4a
Gavin Houston <gavin2024@Gavinspc-Kali.lan>
2025-01-07 19:10:27 -0600

    ops22.py

---
+++ Ops Challenge's/ops22.py
@@ -0,0 +1,44 @@
+# Here is a short story:
+
+# Once upon a time in Appleland, John had three apples, Mary had five apples, and Adam had six apples. They were all very happy and lived for a long time. End of story.
+
+# Your task is to:
+
+# create the variables: john, mary, and adam;
+# assign values to the variables. The values must be equal to the numbers of fruit possessed by John, Mary, and Adam respectively;
+# having stored the numbers in the variables, print the variables on one line, and separate each of them with a comma;
+# now create a new variable named total_apples equal to the addition of the three previous variables.
+# print the value stored in total_apples to the console;
+# experiment with your code: create new variables, assign different values to them, and perform various arithmetic operations on them (e.g., +, -, *, /, //, etc.).
+# Try to print a string and an integer together on one line, e.g., "Total number of apples:" and total_apples.
+
+#1
+john = 3
+mary = 5
+adam = 6
+
+print(john, mary, adam)
+
+total_apples = john + mary + adam
+#2
+print(total_apples)
+
+
+a = 3
+b = 5
+c = 6
+print( a, b, c)
+
+addition = a + b + c
+subtraction = a - b - c
+multiplication = a * b * c
+division = a / b / c
+integer_division = a // b // c
+
+print("total apples:", total_apples)
+
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
+print("Integer Division:", integer_division)
