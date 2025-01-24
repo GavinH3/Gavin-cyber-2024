5a5e9f023b25459881f82cb8761fe37588850c87
Gavin Houston <gavin.houston2006@gmail.com>
2025-01-23 18:48:32 -0600

    ops31.py

--- Ops Challenge's/ops30.py
+++ Ops Challenge's/ops30.py
@@ -5,7 +5,8 @@
 import random
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']3
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
 
 print("Welcome to the PyPassword Generator!")
 nr_letters = int(input("How many letters would you like in your password?\n"))
@@ -14,15 +15,15 @@ nr_numbers = int(input("How many numbers would you like?\n"))
 
 password = []
 
-# Add letters, symbols, and numbers
+
 password += random.choices(letters, k=nr_letters)
 password += random.choices(symbols, k=nr_symbols)
 password += random.choices(numbers, k=nr_numbers)
 
-# Shuffle and create the final password
+
 random.shuffle(password)
 
-# Convert the list to a string
+
 final_password = ''.join(password)
 
 print("Your generated password is:", final_password)
